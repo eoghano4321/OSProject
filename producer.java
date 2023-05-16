3f8be3d5580e3c11a95702794bc139b18fe97000
Derv6464 <dervlag6464@gmail.com>
2023-05-16 15:31:32 +0100

    basic sockets

--- producer.java
+++ producer.java
@@ -1,3 +1,28 @@
+import java.net.*;
+import java.io.*;
+
 public class producer {
-    
+    public void serverSocket(){
+        try{
+            ServerSocket sock = new ServerSocket(6013);
+            // now listen for connections
+            while (true){
+                Socket client = sock.accept();
+                //connection made
+                PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
+                // create menu and send it ...
+                // get input from client for menu item
+                // send back waiting time
+                // wait amount of time
+                // send food done
+
+                pout.println(new java.util.Date().toString());
+                // close the socket and resume listening for more connections
+                client.close();
+            }
+        }
+        catch(IOException ioe){
+            System.err.println(ioe);
+        }
+    }
 }
