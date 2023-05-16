644aa23f4d9941636e7945acbab4a4e28f8bc2be
Eoghan <eoghano4321@gmail.com>
2023-05-16 15:30:27 +0100

    Created basic threads

--- consumer.java
+++ consumer.java
@@ -1,3 +1,18 @@
-public class consumer {
-    
+
+class Consumer implements Runnable
+{
+    private int loop_counter;
+
+    public Consumer(int loops) {
+        loop_counter = loops;
+    }
+
+    public void run() {
+        int num = 0;
+        int val = 0;
+        while(num < loop_counter) {
+            num++;
+        }
+        System.out.println("Consumer finished");
+    }
 }
--- producer.java
+++ producer.java
@@ -1,3 +1,18 @@
-public class producer {
-    
+
+class Producer implements Runnable
+{
+    private int loop_counter;
+
+    public Producer(int loops) {
+        this.loop_counter = loops;
+        
+    }
+
+    public void run() {
+        int num = 0;
+        while(num < loop_counter) {
+            num++;
+        }
+        System.out.println("Producer finished");
+    }
 }
