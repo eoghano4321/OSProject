f715c72aaf11dbbdefc7e8867978de71489f3d4d
Eoghan <eoghano4321@gmail.com>
2023-05-16 16:56:08 +0100

    Added creating and sending menu

--- consumer.java
+++ consumer.java
@@ -1,5 +1,6 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 class Consumer implements Runnable
 {
@@ -15,21 +16,45 @@ class Consumer implements Runnable
         while(num < loop_counter) {
             num++;
         }
+        clientSocket();
         System.out.println("Consumer finished");
     }
     
-     public void clientSocket(){
+    public void clientSocket(){
         try{
             //connect to the server on the local host
             Socket sock = new Socket("127.0.0.1", 6013);
             //set up input stream
             InputStream in = sock.getInputStream();
+
+            ObjectInputStream objectInputStream = new ObjectInputStream(in);
+            Hashtable<Integer, String> menu = null;
+            try {
+                menu = (Hashtable<Integer, String>) objectInputStream.readObject();
+            } catch (ClassNotFoundException e) {
+                // handle class not found exception
+            }
+            // Process the menu data received from the server
+            for (Integer key : menu.keySet()) {
+                System.out.println(key + ". " + menu.get(key));
+            }
+            System.out.println("Please enter the numbers of the item you would like to order: (Press enter after each number and enter 0 when finished)");
+            Scanner scanner = new Scanner(System.in);
+            int input = scanner.nextInt();
+            // Could use a gui to display the menu and get the input instead of the console but we can do that later
+            while (input != 0) {
+                System.out.println("You entered: " + input);
+                input = scanner.nextInt();
+            }
+            scanner.close();
+            // Need to add the inputs to an array and send it to the server for it to process
+
             //read from socket using input stream
-            BufferedReader bin = new BufferedReader(new InputStreamReader(in));
-            //print out the menu
-            String line;
-            while((line = bin.readLine()) != null)
-                System.out.println(line);
+            // BufferedReader bin = new BufferedReader(new InputStreamReader(in));
+
+            // String line;
+            // while((line = bin.readLine()) != null)
+            //     System.out.println(line);
 
             //get input on menu selection
             //send input to server
--- producer.java
+++ producer.java
@@ -1,5 +1,6 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 class Producer implements Runnable
 {
@@ -15,19 +16,29 @@ class Producer implements Runnable
         while(num < loop_counter) {
             num++;
         }
+        System.out.println("Producer started");
+        serverSocket();
         System.out.println("Producer finished");
     
     }
   
-  public void serverSocket(){
+    public void serverSocket(){
         try{
             ServerSocket sock = new ServerSocket(6013);
             // now listen for connections
-            while (true){
-                Socket client = sock.accept();
+            
+            Socket client = sock.accept();
+            OutputStream outputStream = client.getOutputStream();
+            while (client.isConnected()){
                 //connection made
                 PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
-                // create menu and send it ...
+
+                Hashtable<Integer, String> menu = new Hashtable<Integer, String>();
+                menu = createMenu();
+                System.out.println("Menu: " + menu);
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+                objectOutputStream.writeObject(menu);
+
                 // get input from client for menu item
                 // send back waiting time
                 // wait amount of time
@@ -37,9 +48,26 @@ class Producer implements Runnable
                 // close the socket and resume listening for more connections
                 client.close();
             }
+            sock.close();
         }
         catch(IOException ioe){
             System.err.println(ioe);
         }
-  }
+    }
+    
+
+    public Hashtable<Integer, String> createMenu(){
+        Hashtable<Integer, String> menu = new Hashtable<Integer, String>();
+        menu.put(1, "Steak");
+        menu.put(2, "Chicken");
+        menu.put(3, "Pasta");
+        menu.put(4, "Salad");
+        menu.put(5, "Soup");
+        menu.put(6, "Bread");
+        menu.put(7, "Water");
+        menu.put(8, "Soda");
+        menu.put(9, "Beer");
+        menu.put(10, "Wine");
+        return menu;
+    }
 }
